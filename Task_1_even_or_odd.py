ae81b966aaf024143b2d1f78f4336a2093cea007
Harshalgopalani <harshalgopalani@gmail.com>
2025-07-31 12:48:11 +0530

    Add files via upload

---
+++ Task_1_even_or_odd.py
@@ -0,0 +1,13 @@
+number=int(input("Enter a number to check whether it's Even or Odd: "))
+
+if number % 2 == 0:
+    print(number," is Even Number")
+else:
+    print(number," is Odd Number")
+
+#Output:
+# Enter a number to check whether it's Even or Odd: 10
+# 10  is Even Number
+
+# Enter a number to check whether it's Even or Odd: 11
+# 11  is Odd Number
---
+++ Task_2_sum_of_integers_forloop.py
@@ -0,0 +1,9 @@
+counter=0
+
+for number in range(1,51):
+    counter += number
+
+print("The sum from 1 to 50 is:", counter)
+
+#Output:
+# The sum from 1 to 50 is: 1275
